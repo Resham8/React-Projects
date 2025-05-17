f2d943623538bc03fac593e5d3cd486f4dbe3e81
Resham <reshamupadhyay48@gmail.com>
2025-05-17 19:20:58 +0530

    Update AppRoutes.jsx

--- Lamborghini-Landing-Page/src/routes/AppRoutes.jsx
+++ Lamborghini-Landing-Page/src/routes/AppRoutes.jsx
@@ -4,16 +4,18 @@ import Home from "../pages/Home";
 import Beyond from "../pages/Beyond";
 import Museum from "../pages/Museum";
 
-function AppRoutes(){
-return <BrowserRouter>
-    <Routes>
-        <Route element={<MainLayout/>}>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/beyond" element={<Beyond/>}/>
-            <Route path="/museum" element={<Museum/>} />
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/beyond" element={<Beyond />} />
+          <Route path="/museum" element={<Museum />} />
         </Route>
-    </Routes>
-</BrowserRouter>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
