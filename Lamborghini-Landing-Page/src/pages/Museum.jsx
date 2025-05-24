da9a20f132d073d300b2f7c3a076012d9c1dcf68
Resham <reshamupadhyay48@gmail.com>
2025-05-24 23:34:22 +0530

    Update Museum.jsx

--- Lamborghini-Landing-Page/src/pages/Museum.jsx
+++ Lamborghini-Landing-Page/src/pages/Museum.jsx
@@ -26,6 +26,29 @@ export default function Museum() {
           className="object-cover max-h-[700px] w-auto"
         />
       </section>
+      {/* clip-path: polygon(30% 0px, 80% 0px, 62% 100%, 9% 100%); */}
+      <div>
+        <div className="relative w-full h-[500px] overflow-hidden mb-24">
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              clipPath: "polygon(30% 0, 70% 0, 50% 100%, 10% 100%)",
+              WebkitClipPath: "polygon(30% 0, 70% 0, 50% 100%, 10% 100%)",
+            }}
+          >
+            <img
+              src="https://www.lamborghini.com/sites/it-en/files/DAM/lamborghini/face_react/museum/2024/12_18_refresh/s/museo_s1_01.jpg"
+              alt="Lamborghini"
+              style={{
+                objectFit: "cover",
+                width: "100%",
+                height: "100%",
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
